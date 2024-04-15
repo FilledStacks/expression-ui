a9a55d699d5ffcb306f3ffe98eea67335da30c97
Dane Mackier <dane.mackier@outlook.com>
2024-04-15 07:24:42 +0200

    [fl-package] Calls even on postFrameCallback

--- packages/expression-ui-flutter/lib/src/expression_widgets/base_expression_view.dart
+++ packages/expression-ui-flutter/lib/src/expression_widgets/base_expression_view.dart
@@ -2,6 +2,7 @@ import 'package:expression_ui/src/extensions/rive_extensions.dart';
 import 'package:expression_ui/src/models/state_event.dart';
 import 'package:expression_ui/src/utils/state_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:rive/rive.dart';
 
 class BaseExpressionView extends StatelessWidget {
@@ -19,6 +20,7 @@ class BaseExpressionView extends StatelessWidget {
   /// Set to true if the entire view needs to be scrollable
   ///
   /// defaults to false
+  /// Alias for [useArtboardSize]
   final bool scrollable;
 
   /// The name of the state machine to load
@@ -30,15 +32,23 @@ class BaseExpressionView extends StatelessWidget {
   /// Controller to manage the state of [artboardName]
   final StateController? controller;
 
+  final BoxFit fit;
+
+  /// Internally sizes the wdget to match the artboard dimensions defined
+  /// on the canvas
+  final bool useArtboardSize;
+
   const BaseExpressionView({
     super.key,
     required this.filePath,
     required this.artboardName,
     required this.onEvent,
+    this.useArtboardSize = false,
     this.scrollable = false,
     this.stateMachine,
     this.textValues = const {},
     this.controller,
+    this.fit = BoxFit.cover,
   });
 
   @override
@@ -46,8 +56,8 @@ class BaseExpressionView extends StatelessWidget {
     final riveView = RiveAnimation.asset(
       filePath,
       artboard: artboardName,
-      useArtboardSize: scrollable,
-      fit: BoxFit.cover,
+      useArtboardSize: useArtboardSize || scrollable,
+      fit: fit,
       onInit: onRiveFileInitialized,
     );
 
@@ -81,7 +91,9 @@ class BaseExpressionView extends StatelessWidget {
 
     if (stateMachineController != null) {
       stateMachineController.addEventListener((event) {
-        onEvent.call(StateEvent(name: event.name));
+        SchedulerBinding.instance.addPostFrameCallback((_) {
+          onEvent.call(StateEvent(name: event.name));
+        });
       });
 
       controller?.setInputs(stateMachineController.inputs);
