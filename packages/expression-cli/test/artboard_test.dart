b4a9ad1f61bee18905406023b32d12167a310253
Sandip <sandipramanik07@gmail.com>
2024-05-21 10:52:55 +0530

    test: add unit tests for state machine input type, name, value

--- packages/expression-cli/test/artboard_test.dart
+++ packages/expression-cli/test/artboard_test.dart
@@ -1,4 +1,7 @@
 import 'package:expression_cli/src/rive/rive_file.dart';
+import 'package:expression_cli/src/rive/state_machine_bool.dart';
+import 'package:expression_cli/src/rive/state_machine_number.dart';
+import 'package:expression_cli/src/rive/state_machine_trigger.dart';
 import 'package:test/test.dart';
 
 import 'helpers/load_file.dart';
@@ -37,11 +40,36 @@ void main() {
 
   test('Artboard has correct number of state machines', () {
     final screenReaderAB = riveFile.artboardByName('Screen Reader');
-    expect(screenReaderAB!.stateMachines.length, 1); 
+    expect(screenReaderAB!.stateMachines.length, 1);
   });
 
   test('Artboard has correct state machine name', () {
     final screenReaderAB = riveFile.artboardByName('Screen Reader');
-    expect(screenReaderAB!.stateMachines.first.name, 'State Machine 1'); 
+    expect(screenReaderAB!.stateMachines.first.name, 'State Machine 1');
+  });
+
+  test('State Machine has correct inputs', () {
+    final carouselAB = riveFile.artboardByName('Carousel');
+    expect(carouselAB!.stateMachines.length, 1);
+
+    final stateMachine = carouselAB.stateMachines.first;
+    expect(stateMachine.inputs.length, 3);
+
+    final stateMachineInputs = stateMachine.inputs;
+    final boolInputs = stateMachineInputs.whereType<StateMachineBool>();
+    final numberInputs = stateMachineInputs.whereType<StateMachineNumber>();
+    final triggerInputs = stateMachineInputs.whereType<StateMachineTrigger>();
+
+    expect(boolInputs.length, 1);
+    expect(numberInputs.length, 1);
+    expect(triggerInputs.length, 1);
+
+    expect(boolInputs.first.name, 'abc');
+    expect(boolInputs.first.value, false);
+
+    expect(numberInputs.first.name, 'xyz');
+    expect(numberInputs.first.value, 0.0);
+
+    expect(triggerInputs.first.name, 'mno');
   });
 }
