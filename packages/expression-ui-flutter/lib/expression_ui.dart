cd47bd0800004a58d12d80741037b8d633fcfd62
Dane Mackier <dane.mackier@outlook.com>
2025-03-20 09:52:42 +0200

    Fixex compile errir

--- packages/expression-ui-flutter/lib/expression_ui.dart
+++ packages/expression-ui-flutter/lib/expression_ui.dart
@@ -1,6 +1,5 @@
 library;
 
-export 'src/core/expression_manager.dart';
 export 'src/expression_widgets/base_expression_view.dart';
 export 'src/models/state_event.dart';
 export 'src/utils/state_controller.dart';
